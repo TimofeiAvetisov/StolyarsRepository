6c11ae0267bd9088a4028b464cea21cd40d61ae4
Timofey <avetisov.tm@gmail.com>
2021-09-11 22:03:00 +0300

    Added question about vote

--- main.cpp
+++ main.cpp
@@ -6,14 +6,16 @@ using namespace std;
 
 int main(){
     ofstream outf("results.txt");
-    string name, second_name, book;
+    string name, second_name, book, party;
     cout << "Enter your name: ";
     cin >> name;
-    cout << endl << "Enter your second name: ";
+    cout << "Enter your second name: ";
     cin >> second_name;
-    cout << endl << "Enter some book: ";
+    cout << "Enter some book: ";
     getline(cin, book);
     getline(cin, book);
-    outf << "Name: " << name << endl << "Second name: " << second_name << endl << "Some book: " << book << endl;
+    cout << "Enter which party you plan to vote for in: ";
+    getline(cin, party)
+    outf << "Name: " << name << endl << "Second name: " << second_name << endl << "Some book: " << book << endl << "The voter plans to vote for: " << party << endl;
 
 }
