e5277abcbd37b0010750bd9471eb4c373c268233
Timofey <avetisov.tm@gmail.com>
2021-09-11 22:04:35 +0300

    Revert "Added question about vote"
    This reverts commit 6c11ae0267bd9088a4028b464cea21cd40d61ae4.

--- main.cpp
+++ main.cpp
@@ -6,16 +6,14 @@ using namespace std;
 
 int main(){
     ofstream outf("results.txt");
-    string name, second_name, book, party;
+    string name, second_name, book;
     cout << "Enter your name: ";
     cin >> name;
-    cout << "Enter your second name: ";
+    cout << endl << "Enter your second name: ";
     cin >> second_name;
-    cout << "Enter some book: ";
+    cout << endl << "Enter some book: ";
     getline(cin, book);
     getline(cin, book);
-    cout << "Enter which party you plan to vote for in: ";
-    getline(cin, party)
-    outf << "Name: " << name << endl << "Second name: " << second_name << endl << "Some book: " << book << endl << "The voter plans to vote for: " << party << endl;
+    outf << "Name: " << name << endl << "Second name: " << second_name << endl << "Some book: " << book << endl;
 
 }
