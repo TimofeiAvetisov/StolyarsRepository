1b6ef38a04740abd5ac6afcf49ffa1256047aa22
Timofey <avetisov.tm@gmail.com>
2021-09-18 09:08:35 +0300

    Added writing to the end of the file

--- main.cpp
+++ main.cpp
@@ -5,15 +5,16 @@
 using namespace std;
 
 int main(){
-    ofstream outf("results.txt");
+    ofstream outf("results.txt", ios :: app);
     string name, second_name, book;
     cout << "Enter your name: ";
     cin >> name;
-    cout << endl << "Enter your second name: ";
+    cout << "Enter your second name: ";
     cin >> second_name;
-    cout << endl << "Enter some book: ";
+    cout << "Enter some book: ";
     getline(cin, book);
     getline(cin, book);
     outf << "Name: " << name << endl << "Second name: " << second_name << endl << "Some book: " << book << endl;
+    outf.close();
 
 }
