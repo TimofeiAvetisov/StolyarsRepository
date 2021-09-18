07905de1c4a14275c51a05ed35a432d6d043835b
Timofey <avetisov.tm@gmail.com>
2021-09-18 09:22:27 +0300

    Added file output to the screen

--- main.cpp
+++ main.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+void printFile() {
+    fstream inf("results.txt");
+    string name, second_name, book;
+    int i = 0;
+    while (getline(inf, name)) {
+        ++i;
+        getline(inf, second_name);
+        getline(inf, book);
+        cout << "Result number " << i << ": " << endl << name << endl << second_name << endl << book << endl;
+    }
+
+}
+
+
 int main(){
     ofstream outf("results.txt", ios :: app);
     string name, second_name, book;
@@ -16,5 +30,10 @@ int main(){
     getline(cin, book);
     outf << "Name: " << name << endl << "Second name: " << second_name << endl << "Some book: " << book << endl;
     outf.close();
-
+    cout << "Do you wont to see all results?" << endl;
+    string answer;
+    cin >> answer;
+    if (answer == "YES" || answer == "Y" || answer == "yes" || answer == "y" || answer == "Да" || answer == "да") {
+        printFile();
+    }
 }
